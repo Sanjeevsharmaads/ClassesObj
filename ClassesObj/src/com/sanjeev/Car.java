229c26643fc4b24e8e28f15572ebdfd23aaeded6
sanjeevsharma <sanjeev.sharma@affluencesolutions.com>
2018-06-15 14:16:07 +0530

    ClassesObj First

---
+++ ClassesObj/src/com/sanjeev/Car.java
@@ -0,0 +1,12 @@
+package com.sanjeev;
+
+public class Car {
+
+			String color="Black";
+			String model="BMW";
+			
+	public void getValues()
+	{
+		System.out.println("Color of car is: " +color+ " Model is "+model);
+	}
+}
---
+++ ClassesObj/src/com/sanjeev/CarMain.java
@@ -0,0 +1,11 @@
+package com.sanjeev;
+
+public class CarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car obj=new Car();
+		obj.getValues();
+	}
+
+}
