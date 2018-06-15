2b9d4575c3a1e668853cf8f842eb39e8cfe7d85b
sanjeevsharma <sanjeev.sharma@affluencesolutions.com>
2018-06-15 14:21:00 +0530

    update

---
+++ ClassesObj/src/com/sanjeev/Bike.java
@@ -0,0 +1,11 @@
+package com.sanjeev;
+
+public class Bike {
+
+	int price = 50000;
+	String color = "silver";
+
+	public void getValues() {
+		System.out.println("Price of bike is " + price + " Color of bike is  " + color);
+	}
+}
--- ClassesObj/src/com/sanjeev/CarMain.java
+++ ClassesObj/src/com/sanjeev/CarMain.java
@@ -4,8 +4,10 @@ public class CarMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Car obj=new Car();
+		Car obj = new Car();
+		Bike obj1 = new Bike();
 		obj.getValues();
+		obj1.getValues();
 	}
 
 }
