38fb0cbaffc095d8f44faf92ada14ff3b1fff6a8
sanjeevsharma <sanjeev.sharma@affluencesolutions.com>
2018-06-15 16:24:17 +0530

    New Commit Package

---
+++ ClassesObj/src/com/packge5/PersonMain.java
@@ -0,0 +1,14 @@
+package com.packge5;
+
+public class PersonMain {
+
+	public static void main(String[] args) {
+
+		Person person1 = new Person(22, "Sanjeev", "Hisar");
+		Person person2 = new Person(24, "Sahil", "Gurgaon");
+		person1.displayDetails();
+		person2.displayDetails();
+
+	}
+
+}
